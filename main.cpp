cbb6a98833132a188c1303ab494716a4f1013e2c
edwinwijaya94 <edwinwijaya94@yahoo.com>
2016-03-07 14:05:10 +0700

    update

--- main.cpp
+++ main.cpp
@@ -1,8 +1,11 @@
 #include <vector>
+#include <map>
+#include <iostream>
 #include "framebuffer.h"
 #include "input.h"
 #include "color32.h"
 #include "point2.h"
+#include "file.h"
 
 using namespace std;
 
@@ -10,69 +13,61 @@ using namespace std;
 int main(){
 	Framebuffer framebuffer;
 
-	set_conio_terminal_mode();
+	
 
 	framebuffer.ClearScreen();
 	
-	vector<Point2> points; 
-	//points.push_back(Point2(240.0,300.0));
-	points.push_back(Point2(450,400));
-	points.push_back(Point2(480,600));
-	points.push_back(Point2(560,800));
-	points.push_back(Point2(660,600));
-	points.push_back(Point2(690,400));
-
+	vector<string> filenames;
+    filenames.push_back("points.txt");
+	
+	
 	double t;
-
-	for (double i = 0.0; i < 1.0; i+=0.0005) {
-		t = i;
-		framebuffer.drawCurve(points, t, WHITE);
+	
+	map<string, vector<Point2> > points = getPointsFromFile(filenames);
+	//printf("points size: %d\n",points.size());
+	/*for(int i=0; i<points["head_up"].size(); i++)
+		cout<<points["head_up"][i].x<<" "<<points["head_up"][i].y<<endl;*/
+	
+	for(map<string, vector<Point2>>::iterator it=points.begin(); it!=points.end(); it++){
+		//cout<<"it:"<<it->first<<endl;
+		for (double i = 0.0; i < 1.0; i+=0.0005) {
+			t = i;
+			framebuffer.drawCurve(it->second, t, WHITE);
+		}
 	}
 
+	//printf("a\n");
+	vector<Point2> *points_edit;
+	points_edit = &points["hat"];
+	//printf("b\n");
+	Point2 *point_idx;
+	//printf("c\n");
 	framebuffer.SwapBuffers();
-
-	points.clear();
-	points.push_back(Point2(450,400));
-	points.push_back(Point2(530,325));
-	points.push_back(Point2(610,325));
-	points.push_back(Point2(690,400));
-
-	for (double i = 0.0; i < 1.0; i+=0.0005) {
-		t = i;
-		framebuffer.drawCurve(points, t, WHITE);
-	}
-
-	framebuffer.SwapBuffers();
-
-	points.clear();
-	points.push_back(Point2(470,450));
-	points.push_back(Point2(495,425));
-	points.push_back(Point2(520,450));
-
-	for (double i = 0.0; i < 1.0; i+=0.01) {
-		t = i;
-		framebuffer.drawCurve(points, t, WHITE);
-	}
-
-	framebuffer.SwapBuffers();
-
+	set_conio_terminal_mode();
 	while (1) {
 		char c = getch();
-		if(c == 's'){ 
+		//printf("c:  %c",c);
+		if(c == '0')
+			point_idx = &(points_edit->at(0));
+		else if(c == '1')
+			point_idx = &(points_edit->at(1));
+		else if(c == '2')
+			point_idx = &(points_edit->at(2));
+		else if(c == 's'){ 
 			for (double i = 0.0; i < 1.0; i+=0.01) {
 				t = i;
-				framebuffer.drawCurve(points, t, BLACK);
+				framebuffer.drawCurve(*points_edit, t, BLACK);
 			}
 
 			framebuffer.SwapBuffers();
 
-			for (int i = 0; i < points.size(); i++) {
-				points[i].y += 5;
+			for (int i = 0; i < points_edit->size(); i++) {
+				points_edit->at(i).y += 5;
 			}
 
 			for (double i = 0.0; i < 1.0; i+=0.01) {
 				t = i;
-				framebuffer.drawCurve(points, t, WHITE);
+				framebuffer.drawCurve(*points_edit, t, WHITE);
 			}
 
 			framebuffer.SwapBuffers();
@@ -80,18 +75,18 @@ int main(){
 		else if(c == 'w'){ 
 			for (double i = 0.0; i < 1.0; i+=0.01) {
 				t = i;
-				framebuffer.drawCurve(points, t, BLACK);
+				framebuffer.drawCurve(*points_edit, t, BLACK);
 			}
 
 			framebuffer.SwapBuffers();
 
-			for (int i = 0; i < points.size(); i++) {
-				points[i].y -= 5;
+			for (int i = 0; i < points_edit->size(); i++) {
+				points_edit->at(i).y -= 5;
 			}
 
 			for (double i = 0.0; i < 1.0; i+=0.01) {
 				t = i;
-				framebuffer.drawCurve(points, t, WHITE);
+				framebuffer.drawCurve(*points_edit, t, WHITE);
 			}
 
 			framebuffer.SwapBuffers();
@@ -99,18 +94,18 @@ int main(){
 		else if(c == 'a'){ 
 			for (double i = 0.0; i < 1.0; i+=0.01) {
 				t = i;
-				framebuffer.drawCurve(points, t, BLACK);
+				framebuffer.drawCurve(*points_edit, t, BLACK);
 			}
 
 			framebuffer.SwapBuffers();
 
-			for (int i = 0; i < points.size(); i++) {
-				points[i].x -= 5;
+			for (int i = 0; i < points_edit->size(); i++) {
+				points_edit->at(i).x -= 5;
 			}
 
 			for (double i = 0.0; i < 1.0; i+=0.01) {
 				t = i;
-				framebuffer.drawCurve(points, t, WHITE);
+				framebuffer.drawCurve(*points_edit, t, WHITE);
 			}
 
 			framebuffer.SwapBuffers();
@@ -118,18 +113,18 @@ int main(){
 		else if(c == 'd'){ 
 			for (double i = 0.0; i < 1.0; i+=0.01) {
 				t = i;
-				framebuffer.drawCurve(points, t, BLACK);
+				framebuffer.drawCurve(*points_edit, t, BLACK);
 			}
 
 			framebuffer.SwapBuffers();
 
-			for (int i = 0; i < points.size(); i++) {
-				points[i].x += 5;
+			for (int i = 0; i < points_edit->size(); i++) {
+				points_edit->at(i).x += 5;
 			}
 
 			for (double i = 0.0; i < 1.0; i+=0.01) {
 				t = i;
-				framebuffer.drawCurve(points, t, WHITE);
+				framebuffer.drawCurve(*points_edit, t, WHITE);
 			}
 
 			framebuffer.SwapBuffers();
@@ -137,16 +132,16 @@ int main(){
 		else if(c == 'g'){ 
 			for (double i = 0.0; i < 1.0; i+=0.01) {
 				t = i;
-				framebuffer.drawCurve(points, t, BLACK);
+				framebuffer.drawCurve(*points_edit, t, BLACK);
 			}
 
 			framebuffer.SwapBuffers();
 
-			points[1].y += 5;
+			point_idx->y += 5;
 
 			for (double i = 0.0; i < 1.0; i+=0.01) {
 				t = i;
-				framebuffer.drawCurve(points, t, WHITE);
+				framebuffer.drawCurve(*points_edit, t, WHITE);
 			}
 
 			framebuffer.SwapBuffers();
@@ -154,22 +149,25 @@ int main(){
 		else if(c == 't'){ 
 			for (double i = 0.0; i < 1.0; i+=0.01) {
 				t = i;
-				framebuffer.drawCurve(points, t, BLACK);
+				framebuffer.drawCurve(*points_edit, t, BLACK);
 			}
 
 			framebuffer.SwapBuffers();
 
-			points[1].y -= 5;
+			point_idx->y -= 5;
 
 			for (double i = 0.0; i < 1.0; i+=0.01) {
 				t = i;
-				framebuffer.drawCurve(points, t, WHITE);
+				framebuffer.drawCurve(*points_edit, t, WHITE);
 			}
 
 			framebuffer.SwapBuffers();
 		}
 		else if(c == 'x'){
 			break;
+		}
+		else{
+			int idx = int(c);
 		}
 	}
 
--- makefile
+++ makefile
@@ -1,5 +1,5 @@
 all:
-	g++ -o exec main.cpp framebuffer.cpp point2.cpp input.c -lm -std=c++11 -fpermissive
+	g++ -o exec main.cpp framebuffer.cpp point2.cpp input.c file.c -lm -std=c++11 -fpermissive
     
 run:
 	sudo ./exec
